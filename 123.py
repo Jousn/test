c847762e12c4b2d2f5bf1c880e8f6e1b3ac58e8c
eggplant <116062236+Jousn@users.noreply.github.com>
2022-10-18 16:58:38 +0800

    Added a file

---
+++ 123.py
@@ -0,0 +1,162 @@
+
+
+
+
+你好刀客塔
+我是谁
+日期:2022/7/10
+加法
+def get_sum(a,b):
+    result = a + b
+    return result
+
+
+a=1
+b=2
+c= get_sum(a,b)
+print(c)
+
+a=10
+while (a>-10):
+    print(a)
+    a=a-1
+
+print("循环结束")
+
+
+def hanshu2(a,i):
+    while(a<50):
+        a = a ** 2 + 1
+        i = i + 1
+        print(a,i)
+    print("循环结束")
+p=1
+y=0
+hanshu2(p,y)
+
+
+print("在0到10之间")
+a = input("猜猜我是几：")
+guess = int(a)
+if guess == 8:
+    print("猜对了")
+    print("你牛逼")
+else:
+    print("错了")
+
+print("你可以走了")
+
+
+import math
+print(math.pow(2,2))
+
+
+string1="shuhu"
+for string1 in string1:
+    print(string1)
+
+list1=["老邓","老张","老王"]
+for person in list1:
+    print(person)
+
+
+for i in range(0,10,2):
+    print(i)
+
+
+x = input("起始值；")
+a = int(x)
+def hanshu1 (a,i=0):
+    print(a,0)
+    while (a < 70):
+        a=a*2+2
+        i=i+1
+        print(a,i)
+    print("循环结束")
+
+
+hanshu1(a,i=0)
+
+
+import turtle
+
+turtle.showturtle()
+turtle.back(50)
+turtle.forward(100)
+
+
+turtle.left(90)
+turtle.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
